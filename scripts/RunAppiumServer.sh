2817a1acc1fc8cccecf088ab0c4bae0c521faf53
Md. Mahbubur Rahman <mahbubasr091@gmail.com>
2023-08-13 03:05:38 +0600

    update

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -3,7 +3,7 @@ set -ex
 npm install -g appium@next
 appium driver install xcuitest
 appium driver install espresso
-appium driver install flutter
+appium driver install uiautomator2
 
 appium -v
 appium --log appium.log &>/dev/null &
